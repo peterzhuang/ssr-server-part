523dae6cc59ded6ed1ac9a22839290dc07d751bc
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2019-12-17 15:09:41 -0500

    L21 merge webpack config

--- webpack.client.js
+++ webpack.client.js
@@ -1,7 +1,8 @@
 const path = require('path');
-const webpackNodeExternals = require('webpack-node-externals');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const config = {
 
     // Tell webpack the root file of our server application
     entry:  './src/client/client.js',
@@ -10,26 +11,28 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
-    },
-
-    // Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        '@babel/react',
-                        ['@babel/env', { targets: { browsers: ['last 2 versions'] } }]
-                    ],
-                    plugins: [
-                        [ "@babel/plugin-proposal-class-properties" ]
-                    ],
-                }
-            }
-        ]
     }
 
+    // Tell webpack to run babel on every file it runs through
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js?$/,
+    //             loader: 'babel-loader',
+    //             exclude: /node_modules/,
+    //             options: {
+    //                 presets: [
+    //                     '@babel/react',
+    //                     ['@babel/env', { targets: { browsers: ['last 2 versions'] } }]
+    //                 ],
+    //                 plugins: [
+    //                     [ "@babel/plugin-proposal-class-properties" ]
+    //                 ],
+    //             }
+    //         }
+    //     ]
+    // }
+
 };
+
+module.exports = merge(baseConfig, config);
--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 const webpackNodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const config = {
     // Inform webpack that we're building bundle for nodeJS, rather than for the browser
 
     target: 'node',
@@ -15,26 +17,28 @@ module.exports = {
         path: path.resolve(__dirname, 'build')
     },
 
-    externals: [webpackNodeExternals()],
+    externals: [webpackNodeExternals()]
 
     // Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        '@babel/react',
-                        ['@babel/env', { targets: { browsers: ['last 2 versions'] } }]
-                    ],
-                    plugins: [
-                        [ "@babel/plugin-proposal-class-properties" ]
-                    ],
-                }
-            }
-        ]
-    }
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js?$/,
+    //             loader: 'babel-loader',
+    //             exclude: /node_modules/,
+    //             options: {
+    //                 presets: [
+    //                     '@babel/react',
+    //                     ['@babel/env', { targets: { browsers: ['last 2 versions'] } }]
+    //                 ],
+    //                 plugins: [
+    //                     [ "@babel/plugin-proposal-class-properties" ]
+    //                 ],
+    //             }
+    //         }
+    //     ]
+    // }
 
 };
+
+module.exports = merge(baseConfig, config);
