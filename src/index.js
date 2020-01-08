9cbea18d49b466d5a860f38d8f482a3575805abb
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2020-01-08 17:20:37 -0500

    start L35 FetchUsers Action creator

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 // import React from 'react';
 // import { renderToString } from 'react-dom/server';
 // import Home from './client/components/Home';
@@ -14,9 +15,11 @@ const app  = express();
 app.use(express.static('public'));
 
 app.get('*', (req,res) => {
+    const store = createStore();
 
+    // some logic to initialize and load data into store
 
-    res.send(renderer(req));
+    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
